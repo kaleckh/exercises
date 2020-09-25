b136ce503574450a8140ebc32af4dd0e8b86e952
Jason Hamm <jasonhamm.me@gmail.com>
2020-09-25 07:13:53 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var mock = require("./mock")
+
+// filter 
+// What does filter return?
+// What is the param that is passed to .filter?
+// Filter the array to only items where first_name starts with 'A'
+
+
+// map
+// What does map return?
+// What is the param that is passed to .map?
+// Map the array to be true for even id's and false for odd id's
+
+
+// findIndex
+// What does findIndex return?
+// What is the params that is passed to .findIndex
+// find the index of the item with the first_name is 'Blondelle'
+
+
+// includes
+// What does includes return?
+// What is the param that is passed to .includes
+// find if the data contains a last_name is 'Fenelow'
+
+
